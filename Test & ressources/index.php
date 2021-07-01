b5318c530194741f769a3ce6ac2711df1e3ec6bf
Jawhar Dev <lavoieexpressmaroc@gmail.com>
2021-07-01 18:42:37 +0100

    test

--- Test & ressources/index.php
+++ Test & ressources/index.php
@@ -1,34 +1,155 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script src="VUEJS/vue.js"></script>
+<table border="1" id="crudApp">
+    <thead>
+        <tr>
+            <th>Numero</th>
+            <th>Date</th>
+            <th>Poids</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="declaration in allData">
+            <th>{{declaration.numero}}</th>
+            <th>{{declaration.date}}</th>
+            <th>{{declaration.poids}}</th>
+        </tr>
+    </tbody>
+</table>
+<script>
+    var application = new Vue({
+        el: '#crudApp',
+        data: {
+            allData: '',
+            myModel: false,
+            actionButton: 'Insert',
+            dynamicTitle: 'Add Data',
+        },
+        methods: {
+            fetchAllData: function() {
+                axios.post('test.php', {
+                    id: '1',
+                    action: 'insert'
+                }).then(function(response) {
+                    application.allData = response.data;
+                });
+            },
+            openModel: function() {
+                application.first_name = '';
+                application.last_name = '';
+                application.actionButton = "Insert";
+                application.dynamicTitle = "Add Data";
+                application.myModel = true;
+            },
+            submitData: function() {
+                if (application.first_name != '' && application.last_name != '') {
+                    if (application.actionButton == 'Insert') {
+                        axios.post('action.php', {
+                            action: 'insert',
+                            firstName: application.first_name,
+                            lastName: application.last_name
+                        }).then(function(response) {
+                            application.myModel = false;
+                            application.fetchAllData();
+                            application.first_name = '';
+                            application.last_name = '';
+                            alert(response.data.message);
+                        });
+                    }
+                    if (application.actionButton == 'Update') {
+                        axios.post('action.php', {
+                            action: 'update',
+                            firstName: application.first_name,
+                            lastName: application.last_name,
+                            hiddenId: application.hiddenId
+                        }).then(function(response) {
+                            application.myModel = false;
+                            application.fetchAllData();
+                            application.first_name = '';
+                            application.last_name = '';
+                            application.hiddenId = '';
+                            alert(response.data.message);
+                        });
+                    }
+                } else {
+                    alert("Fill All Field");
+                }
+            },
+            fetchData: function(id) {
+                axios.post('action.php', {
+                    action: 'fetchSingle',
+                    id: id
+                }).then(function(response) {
+                    application.first_name = response.data.first_name;
+                    application.last_name = response.data.last_name;
+                    application.hiddenId = response.data.id;
+                    application.myModel = true;
+                    application.actionButton = 'Update';
+                    application.dynamicTitle = 'Edit Data';
+                });
+            },
+            deleteData: function(id) {
+                if (confirm("Are you sure you want to remove this data?")) {
+                    axios.post('action.php', {
+                        action: 'delete',
+                        id: id
+                    }).then(function(response) {
+                        application.fetchAllData();
+                        alert(response.data.message);
+                    });
+                }
+            }
+        },
+        created: function() {
+            this.fetchAllData();
+        }
+    });
+</script>
 
 <script>
-    /*  
-    axios.get('test.php', {
-            params: {
-                id: 1
-            }
+    /*axios.post('test.php', {
+            id: 1
         }, {
             headers: {
                 'Conent-type': 'application/json'
             }
         })
         .then((response) => {
-
-            console.log(response.data);
+            $.each(response.data, function(index, declaration) {
+                console.log(declaration.poids);
+            });
         }).catch(err => {
             console.log(err);
         });*/
+
+    /* axios.get('test2.php', {
+             params: {
+                 id: 1
+             }
+         }, {
+             headers: {
+                 'Conent-type': 'application/json'
+             }
+         })
+         .then((response) => {
+             $.each(response.data, function(index, declaration) {
+                 $('tbody').append('<tr><td>' + declaration.numero + '</td><td>' + declaration.date + '</td><td>' + declaration.poids + '</td>');
+                 // console.log(declaration.poids);
+             });
+         }).catch(err => {
+             console.log(err);
+         });*/
 </script>
 <script>
+    /*
     fetch('http://api.relaisexpress.ma/v1/getconsigne', {
     method: 'GET',
     headers: new Headers({
         'Authorization': 'Basic ' + Buffer.from("lve:kC525bmT").toString('base64'),
         'Content-Type': 'application/json'
     }),
-    })
-
-
+    })*/
     /* fetch('http://api.relaisexpress.ma/v1/getconsigne', {
              method: 'GET',
              headers: new Headers({
@@ -72,12 +193,12 @@
                 }*/
     /*, {
                     
-                }*/
+                }
     )
     .then((response) => {
         console.log(response);
     });
-
+*/
     /* $.ajax({
         type: "GET",
         url: "http://api.relaisexpress.ma/v1/getconsigne",
--- Test & ressources/test2.php
+++ Test & ressources/test2.php
@@ -3,17 +3,9 @@ $connection = new PDO('mysql:dbname=taxation;host=localhost', 'root', '');
 #$result = $connection->query("SELECT * FROM declaration_v");
 #echo json_encode($result->fetchAll()); 
 $error = array('error' => 404, 'message' => "Id introuvable");
-/*if (isset($_GET['id'])) {
+if (isset($_GET['id'])) {
     $result = $connection->prepare("SELECT * FROM declaration_v where client1_id=?");
     echo ($result->execute([$_GET['id']])) ? json_encode($result->fetchAll()) : json_encode($error);
-} else {
-    echo json_encode($error);
-}
-*/
-$recieved_data = json_decode(file_get_contents("php://input"));
-if (isset($recieved_data->id)) {
-    $result = $connection->prepare("SELECT * FROM declaration_v where client1_id=?");
-    echo ($result->execute([$recieved_data->id])) ? json_encode($result->fetchAll()) : json_encode($error);
 } else {
     echo json_encode($error);
 }
