e451d1ce134bca43728c6d51f0e94160c9d107b4
Jawhar Dev <lavoieexpressmaroc@gmail.com>
2021-08-02 18:22:38 +0100

    another commit

---
+++ Taxation/Taxation_laravel/vfw/resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,11 @@
+<div class="lve-sidebar" style="width:25%">
+    <ul>
+        <li><a href="#" class="lve-bar-item <?= $Con_active; ?>"><i class="fas fa-eye"></i> Consultation</a></li>
+        <li><a href="#" class="lve-bar-item <?= $Déc_active; ?>"><i class="fas fa-file-alt"></i> Déclarations</a></li>
+        <li><a href="#" class="lve-bar-item <?= $Ram_active; ?>"><i class="fas fa-truck"></i>Ramassage (0)</a></li>
+        <li><a href="#" class="lve-bar-item <?= $Sui_active; ?>"><i class="fas fa-map-marker-alt"></i> Suivi des envois</a></li>
+        <li><a href="#" class="lve-bar-item <?= $Eta_active; ?>"><i class="fas fa-cubes"></i> Etat de stock</a></li>
+        <li><a href="#" class="lve-bar-item <?= $Mes_active; ?>"><i class="fas fa-users"></i> Mes sessions</a></li>
+        <li><a href="#" class="lve-bar-item <?= $Rec_active; ?>"><i class="fas fa-headset"></i> Réclamations</a></li>
+    </ul>
+</div>
